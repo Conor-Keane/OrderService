7da17ab92888e71d4c61d808733126f464fbed40
conor-keane <g00391846@atu.ie>
2023-11-08 09:25:26 +0000

    Added GetMapping and PostMapping to OrderController

--- src/main/java/ie/atu/orderservice/orderservice/OrderController.java
+++ src/main/java/ie/atu/orderservice/orderservice/OrderController.java
@@ -1,4 +1,29 @@
 package ie.atu.orderservice.orderservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class OrderController {
+    private OrderService orderService;
+
+    @Autowired
+    public void setOrderService(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/addOrder")
+    public  String addOrder(@RequestBody OrderDetails orderDetails) {
+        orderService.addOrder(orderDetails);
+        return "Order created";
+    }
+
+    @GetMapping("/getOrder")
+    public @RequestBody List<OrderService>getOrder() {
+        return orderService.getOrder();
+    }
+
 }
