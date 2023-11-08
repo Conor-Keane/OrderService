da261ff0cdcc6f214c571f4fa82f756c2d138445
conor-keane <g00391846@atu.ie>
2023-11-08 09:15:21 +0000

    Added Data to customerId

--- src/main/java/ie/atu/orderservice/orderservice/OrderDetails.java
+++ src/main/java/ie/atu/orderservice/orderservice/OrderDetails.java
@@ -1,4 +1,23 @@
 package ie.atu.orderservice.orderservice;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDetails {
+    @JsonProperty("orderId")
+    private int orderId;
+
+    @JsonProperty("productId")
+    private int productId;
+
+    @JsonProperty("quantity")
+    private int quantity;
+
+    @JsonProperty("customerId")
+    private int customerId;
 }
