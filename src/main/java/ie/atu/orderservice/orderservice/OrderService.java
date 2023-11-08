fdd5ad1c563327db83f86f240843369a9faa7c55
conor-keane <g00391846@atu.ie>
2023-11-08 09:12:00 +0000

    Created OrderDetails class to store data

--- src/main/java/ie/atu/orderservice/orderservice/OrderService.java
+++ src/main/java/ie/atu/orderservice/orderservice/OrderService.java
@@ -1,4 +1,12 @@
 package ie.atu.orderservice.orderservice;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "Order-service", url = "http://localhost:8081")
 public interface OrderService {
+    @PostMapping("/createOrder")
+    String orderDetails();
+
 }
